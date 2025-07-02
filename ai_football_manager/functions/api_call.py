42cbb6f386e9717f79334de8d4c0443c258d6aaf
changminbyeon <yhi021377@gmail.com>
2025-07-03 00:59:54 +0900

    feat: 함수 추가 및 수정(api_call.py)

--- ai_football_manager/functions/api_call.py
+++ ai_football_manager/functions/api_call.py
@@ -1,4 +1,5 @@
 import streamlit as st
+# from tool_call_handler import tools
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
@@ -6,13 +7,20 @@ import os
 load_dotenv()
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
-def openai_call():
+def web_search_4o(messages:list):
     response = client.chat.completions.create(
-       model=st.session_state.openai_model,
-        messages=[
-            {"role": m['role'], "content": m['content']}
-            for m in st.session_state.messages
-        ],
+        model=st.session_state.openai_model,
+        messages=messages,
+        tools=[{"type": "web_search_preview"}],
+        stream=True
+    )
+
+    return response
+
+def openai_call(messages:list):
+    response = client.chat.completions.create(
+        model=st.session_state.openai_model,
+        messages=messages,
         stream=True
     )
     
