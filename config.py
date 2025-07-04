1dc41d9a302afe74bc353f3d7ff51abc0606c1a7
changminbyeon <yhi021377@gmail.com>
2025-07-04 17:01:45 +0900

    feat: 검색을 위한 API 추가

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+NEWS_API_KEY = os.getenv('NEWS_API_KEY')
+X_RAPIDAPI_KEY = os.getenv('X_RAPIDAPI_KEY')
